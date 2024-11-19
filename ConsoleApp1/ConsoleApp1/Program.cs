571ea18789d8609acbaa1d08d45a90d6c89380d2
Ghostmatvey <mokroysov2006@gmail.com>
2024-11-19 14:38:44 +0900

    Правка конечная

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Sum
+{
+    public static double sum(double a, double b)
+    {
+        return a + b;
+    }
+
+ 
+    private static double droub(double a, double b)
+    {
+        return (a + b) / 2;
+    }
+
+    public static void Main(string[] args)
+    { double a,b,c,d;
+        
+         a= double.Parse(Console.ReadLine());
+         b= double.Parse(Console.ReadLine());
+       c = sum(a, b);
+        d= droub(a, b);
+        Console.WriteLine($"Сумма: {c}");
+        Console.WriteLine($"Среднее арифметическое: {d}");
+    }
+}
+
+class piutnibvhjbvff
+{
+    public static void Main(string[] args)
+    {
+        double a, g, c;
+        a= double.Parse(Console.ReadLine()); g= double.Parse(Console.ReadLine());
+        Console.WriteLine(Sum.sum(a, g));
+    }
+}
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Методы и классы")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Методы и классы")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Методы и классы")]
+[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
